636923f6f7430f692610cfda130c61df542c1eba
Thami-exp <60342970+Thami-exp@users.noreply.github.com>
2020-02-12 13:03:16 +0200

    Create Metric_Dictionary.py

---
+++ Metric_Dictionary.py
@@ -0,0 +1,11 @@
+def dictionary_of_metrics(items):
+    
+    n   = len(items)
+    Ave = round(np.mean(items),2)
+    Med = round(np.median(items),2)
+    Var = round((sum((items-np.mean(items))**2))/(n-1),2)
+    Std = round(((sum((items-np.mean(items))**2))/(n-1))**(1/2),2)
+    Min = round(min(items),2)
+    Max = round(max(items),2)
+    
+    return {'mean':Ave,'median':Med,'var':Var,'std':Std,'min':Min,'max':Max}
