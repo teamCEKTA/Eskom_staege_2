c62d12fc3abcd645d7dc86b1224f50948f9ad5c0
keithtakarinda <60345058+keithtakarinda@users.noreply.github.com>
2020-02-14 13:09:19 +0200

    adjust function name

--- stop_words_remover.py
+++ stop_words_remover.py
@@ -1,6 +1,6 @@
 #Function 7
 
-def stop_words_http_remover(df):
+def stop_words_remover(df):
     """ A function that takes a pandas dataframe as an input, tokenises the
     sentences and removes all the stopwords in the input using a stop words
     dictionary and returns a modified version of the pandas dataframe
