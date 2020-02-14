e3adef70260e1d67781f9f47f500806abd127155
Amogelang Mpyatona <amogelang1996@gmail.com>
2020-02-14 08:29:15 +0200

    Update number_of_tweets_per_day.py

--- number_of_tweets_per_day.py
+++ number_of_tweets_per_day.py
@@ -1,16 +1,13 @@
 def number_of_tweets_per_day(df):
   ### Code Here
     #df = df['Date'].to_list()
-    b = pd.DataFrame([item.split()[0] for item in df],columns=["Date"])
-
-    date = pd.DataFrame(b['Date'].value_counts(),columns=["Date","Tweets"])
-
-    date.reset_index(inplace=True)
-
-    date.drop(['Tweets'],axis=1,inplace=True)
-
-    date.columns = ['Date', 'Tweets']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] =  df['Date'].dt.date                            
+    #twitter_df['Date'] = pd.DataFrame([item.split()[0] for item in dates],columns=["Date"])   
+    twitter_cnt=twitter_df.groupby('Date').count()
+    twitter_cnt.reset_index(inplace=True)
+    twitter_cnt.set_index('Date', inplace=True)   
     
-    #date.sort_values('Date')
-    
-    return date
+    return twitter_cnt
+
+number_of_tweets_per_day(twitter_df)
