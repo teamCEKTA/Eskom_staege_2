0343abb840221d4c522b64ae5365d8cf2db91e4b
keithtakarinda <60345058+keithtakarinda@users.noreply.github.com>
2020-02-14 13:03:52 +0200

    remove http remover

--- stop_words_remover.py
+++ stop_words_remover.py
@@ -1,10 +1,14 @@
 #Function 7
-""" A function that takes a pandas dataframe as an input, tokenises the sentences and removes all the stopwords in the input using a stop words dictionary and returns a modified version of the pandas dataframe
-    """
+
 def stop_words_http_remover(df):
+    """ A function that takes a pandas dataframe as an input, tokenises the
+    sentences and removes all the stopwords in the input using a stop words
+    dictionary and returns a modified version of the pandas dataframe
+    """
     df['Without Stop Words'] = df['Tweets'].apply(str.lower).apply(str.split)
 
     for i in range(len(twitter_df)):
         df['Without Stop Words'][i] = [x for x in df['Without Stop Words'][i] if x not in stop_words_dict['stopwords']]
-        df['Without Stop Words'][i] = [x for x in df['Without Stop Words'][i] if "http" not in x]
+    return df
     pass
+    
