0a87eff7e7b1fb837105010b960de73cb68f7db8
keithtakarinda <60345058+keithtakarinda@users.noreply.github.com>
2020-02-19 10:17:51 +0200

    package trial 3

--- first package/setup.py
+++ first package/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='e_Functions_package',
+    name='mypackage',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
