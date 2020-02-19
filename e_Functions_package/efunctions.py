36659be24fdf9108146a8784f679c0580363e8d3
Amogelang20 <60345817+Amogelang20@users.noreply.github.com>
2020-02-19 11:05:26 +0200

    Update efunctions.py

--- e_Functions_package/efunctions.py
+++ e_Functions_package/efunctions.py
@@ -1,11 +1,10 @@
 def dictionary_of_metrics(items):
     """Takes a list of items and returns a dictionary of the summary statistics"""
-    import numpy
     n = len(items)
-    average = round(numpy.mean(items), 2)
-    median = round(numpy.median(items), 2)
-    variance = round((sum((items-numpy.mean(items))**2))/(n-1), 2)
-    standard_dev = round(((sum((items-numpy.mean(items))**2))/(n-1))**(1/2), 2)
+    average = round(np.mean(items), 2)
+    median = round(np.median(items), 2)
+    variance = round((sum((items-np.mean(items))**2))/(n-1), 2)
+    standard_dev = round(((sum((items-np.mean(items))**2))/(n-1))**(1/2), 2)
     minimum = round(min(items), 2)
     maximum = round(max(items), 2)
     
